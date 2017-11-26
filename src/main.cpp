ce059bfe1cf6801e892507ddf1b166328a74ee5c
tsunekawa0317 <34001540+tsunekawa0317@users.noreply.github.com>
2017-11-26 23:47:19 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,16 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   // start from 1 to ignore script name; argv[0] will be a name of processing file.
-  for (int i = 1; i < argc; i++) {
-    if((argv[1]=="World") ||(argv[1]=="織田信長") || (argv[1]=="codecheck")){
-      printf("Hello %s!",argv[1]);
+  printf("How many characters did you input?");
+  scanf("%d",&argc);
+
+  printf("Enter a word, prefering 'World', '織田信長', or 'codecheck'.");
+  for(int i=1;i<argc+1;i++){
+    scanf("%s",&argv[i]);
+  }
+  for (int i = 1; i < argc+1; i++) {
+    if((argv[1]=="W") ||(argv[1]=="織") || (argv[1]=="c")){
+      printf("Hello %s!",argv[i]);
     }
     else if(argv[1]==NULL){
       printf("Hello!");
