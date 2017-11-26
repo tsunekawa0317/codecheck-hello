9ead911bdb8c8741c24a37ee2f71a06b51af912e
tsunekawa0317 <34001540+tsunekawa0317@users.noreply.github.com>
2017-11-26 23:34:36 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ int main(int argc, char *argv[])
 {
   // start from 1 to ignore script name; argv[0] will be a name of processing file.
   for (int i = 1; i < argc; i++) {
+    if((argv[1]=="World") ||(argv[1]=="織田信長") || (argv[1]=="codecheck"){
+      printf("Hello %s!",argv[1]);
+    }
+    else if(argv[1]==NULL){
+      printf("Hello!");
+    }
     printf ("argv[%i]: %s\n", i, argv[i]);
   }
   return 0;
